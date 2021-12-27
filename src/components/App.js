7c31695b7d4fdf5fedddf4cbbd261b15a39e3463
David <davidchedrick@gmail.com>
2021-12-27 09:53:26 -0600

    commit tree fc76f5a9ace923f2a11a5a707ce832c32ba778cb

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+
+  const [wallet, setWallet] = useState(100);
+  const [sushis, setSushis] = useState([]);
+  console.log("sushis: ", sushis);
+
+  useEffect(() => {
+    fetch(API)
+    .then(resp => resp.json())
+    .then(sushis => {
+      const updatedSushis = sushis.map(sushi => {
+        return { ...sushi, eaten: false };
+      });
+      setSushis(updatedSushis)
+    })
+  }, []);
+
+  function handleEatSushi(eatenSushi) {
+    if(wallet >= eatenSushi.price) {
+      const updatedSushis = sushis.map(sushi => {
+        if(sushi.id === eatenSushi.id) return { ...sushi, eaten:true};
+        return sushi;
+      });
+      setSushis(updatedSushis);
+      setWallet(wallet => wallet - eatenSushi.price);
+    } else {
+      alert("Need more 💸");
+    }
+  }
+
+  function handleAddMoney(moreMoney) {
+    setWallet(wallet => wallet + moreMoney)
+  }
+
+  const eatenSushi = sushis.filter(sushi => sushi.eaten)
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer 
+        sushis={sushis} 
+        onEatSushi={handleEatSushi}
+      />
+      <Table 
+        wallet={wallet} 
+        onAddMoney={handleAddMoney}
+        plates={eatenSushi}
+      />
     </div>
   );
+
 }
 
 export default App;
--- src/components/Sushi.js
+++ src/components/Sushi.js
@@ -1,23 +1,35 @@
 import React from "react";
 
-function Sushi(props) {
+function Sushi({ sushi, onEatSushi }) {
+
+  const { img_url, name, price, eaten } = sushi;
+  console.log("eaten: ", eaten);
+
+  function handleCLick() {
+    if (!eaten) {
+      onEatSushi(sushi);
+    } else {
+      alert("Cant eat an empty plate, bud!");
+    }
+  }
+
   return (
     <div className="sushi">
-      <div className="plate" onClick={/* Give me a callback! */ null}>
-        {/* Tell me if this sushi has been eaten! */}
-        {false ? null : (
+      <div className="plate" onClick={handleCLick}>
+        {eaten ? null : (
           <img
-            src={/* Give me an image source! */ null}
-            alt={/* Give me a name! */ "Sushi"}
+            src={img_url}
+            alt={name}
             width="100%"
           />
         )}
       </div>
       <h4 className="sushi-details">
-        {/* Give me a name! */} - ${/* Give me a price! */}
+        {name} - ${price}
       </h4>
     </div>
   );
+
 }
 
 export default Sushi;
